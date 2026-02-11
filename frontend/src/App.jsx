1bca5f4aaf403af40703723fe5d15e3d5a42a5d6
logancostello <locostel@calpoly.edu>
2026-02-10 19:10:35 -0800

    add: hello world

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Sound Ethics</h1>
+      <p>hello world</p>
+    </>
+  )
+}
+
+export default App
